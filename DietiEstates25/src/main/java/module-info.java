c6f299007869aef3f2f6053d8df102f0641f306f
carla <carla@localhost>
2024-11-30 11:59:23 +0100

    UserLoginPageCommit : Nuova grafica,Nuovi pulsanti,controller creato, privo di funzionalità!

--- DietiEstates25/src/main/java/module-info.java
+++ DietiEstates25/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.openjfx.DietiEstates25 {
     requires javafx.controls;
     requires javafx.fxml;
+	requires javafx.graphics;
 
     opens org.openjfx.DietiEstates25 to javafx.fxml;
     exports org.openjfx.DietiEstates25;
---
+++ DietiEstates25/src/main/java/org/openjfx/DietiEstates25/LoginPageUsersController.java
@@ -0,0 +1,35 @@
+package org.openjfx.DietiEstates25;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+
+public class LoginPageUsersController {
+	@FXML
+	private Button ButtonAccediUsers;
+	@FXML
+	private Button ButtonRicordaUsers;
+	@FXML
+	private Button ButtonGoogleUsers;
+	@FXML
+	private ImageView Google;
+	@FXML
+	private Button ButtonFBUsers;
+	@FXML
+	private ImageView Fb;
+	@FXML
+	private Button ButtonGitUsers;
+	@FXML
+	private ImageView Git;
+	@FXML
+	private Button ButtonInfoUsers;
+	@FXML
+	private ImageView Back;
+	@FXML
+	private Button ButtunBackUsers;
+	
+	
+	
+	
+ 
+}
